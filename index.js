14cdbfd59468a20c44c97315dc56ce51a8368d17
Zx-Akito <zxakito89@gmail.com>
2024-08-06 15:15:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const client = new Client({
         type: 'remote', 
         remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
     },
-    ffmpeg: './ffmpeg.exe',
     authStrategy: new LocalAuth({ clientId: "client" })
 });
 const config = require('./config/config.json');
@@ -26,105 +25,17 @@ client.on('qr', qr => {
 });
 
 client.on('ready', () => {
-    console.clear()
-    // const consoleTextPath = path.join(__dirname, 'config', 'console.txt');
-    // fs.readFile(consoleTextPath, 'utf-8', (err, data) => {
-    //     if (err) {
-    //         console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Console Text not found!`.yellow);
-    //     } else {
-    //         console.log(data.green);
-    //     }
-    // });
+    console.clear();
     console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Ready!`.green);
 
     // Jadwalkan pengiriman pesan
     scheduleMessages([
-        { time: '08:00', groupId: '120363177308785364@g.us', message: 'Selamat pagi, Rifki!' },
-        { time: '08:00', groupId: '120363177308785364@g.us', message: 'Selamat pagi, Galuh!' },
-        { time: '08:00', groupId: '120363177308785364@g.us', message: 'Selamat pagi, Wildan!' },
-        { time: '12:00', groupId: '120363177308785364@g.us', message: 'Selamat siang, Rifki!' },
-        { time: '12:00', groupId: '120363177308785364@g.us', message: 'Selamat siang, Galuh!' },
-        { time: '12:00', groupId: '120363177308785364@g.us', message: 'Selamat siang, Wildan!' },
-        { time: '17:00', groupId: '120363177308785364@g.us', message: 'Selamat sore, Rifki!' },
-        { time: '17:00', groupId: '120363177308785364@g.us', message: 'Selamat sore, Galuh!' },
-        { time: '17:00', groupId: '120363177308785364@g.us', message: 'Selamat sore, Wildan!' },
-        { time: '22:00', groupId: '120363177308785364@g.us', message: 'Selamat malam, Rifki!' },
-        { time: '22:00', groupId: '120363177308785364@g.us', message: 'Selamat malam, Galuh!' },
-        { time: '22:00', groupId: '120363177308785364@g.us', message: 'Selamat malam, Wildan!' }
+        { time: '08:00', groupId: '120363177308785364@g.us', message: 'Selamat pagi, Rifki Sayang!' },
+        { time: '12:00', groupId: '120363177308785364@g.us', message: 'Selamat siang, Rifki Sayang!' },
+        { time: '17:00', groupId: '120363177308785364@g.us', message: 'Selamat sore, Rifki Sayang!' },
+        { time: '22:00', groupId: '120363177308785364@g.us', message: 'Selamat malam, Rifki Sayang!' },
     ]);
 });
-
-client.on('message', async message => {
-    const isGroup = message.from.endsWith('@g.us');
-    console.log(message.from);
-    if (!isGroup && !config.groups) return;
-
-    switch (message.body.toLowerCase()) {
-        case `${config.prefix}sticker`:
-            handleStickerCreation(message);
-            break;
-        case `${config.prefix}image`:
-            handleStickerToImage(message);
-            break;
-        default:
-            markChatAsSeen(message);
-    }
-});
-
-// Function to create sticker
-const handleStickerCreation = async (message) => {
-    try {
-        const media = await getMediaFromMessage(message);
-        if (media) {
-            await client.sendMessage(message.from, media, {
-                sendMediaAsSticker: true,
-                stickerName: config.name,
-                stickerAuthor: config.author
-            });
-            console.log(message.from.yellow, "Success to create sticker!".green);
-        } else {
-            await client.sendMessage(message.from, "Reply with an image to create a sticker!");
-        }
-    } catch (error) {
-        console.error(error);
-        await client.sendMessage(message.from, "Failed to create sticker!");
-        console.log(message.from.yellow, "Failed to create sticker!".red);
-    }
-};
-
-// Function to convert sticker to image
-const handleStickerToImage = async (message) => {
-    try {
-        const media = await getMediaFromMessage(message);
-        if (media) {
-            await client.sendMessage(message.from, media);
-            await client.sendMessage(message.from, "Success to convert image!");
-            console.log(message.from.red, "Success to convert image!");
-        }
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-// Utility function to get media from message
-const getMediaFromMessage = async (message) => {
-    if (message.hasQuotedMsg) {
-        const quotedMsg = await message.getQuotedMessage();
-        if (quotedMsg.hasMedia) {
-            return await quotedMsg.downloadMedia();
-        }
-    }
-    if (message.hasMedia) {
-        return await message.downloadMedia();
-    }
-    return null;
-};
-
-// Function to mark chat as seen
-const markChatAsSeen = async (message) => {
-    const chat = await client.getChatById(message.id.remote);
-    await chat.sendSeen();
-};
 
 // Fungsi untuk menjadwalkan beberapa pengiriman pesan ke grup
 const scheduleMessages = (messages) => {
